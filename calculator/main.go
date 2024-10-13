5127438cb7a38227feba575c783239653449473f
Luis Carreira <luiseducarreira@gmail.com>
2024-10-13 18:42:30 +0000

    feat/calculator: using interface for swapping managers

--- calculator/main.go
+++ calculator/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
-
-	"example.com/exercises/calculator/filemanager"
+	"example.com/exercises/calculator/cmdmanager"
 	"example.com/exercises/calculator/prices"
 )
 
@@ -11,8 +9,10 @@ func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		cmdm := cmdmanager.New()
+		// priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
 		priceJob.Process()
 	}
 }
